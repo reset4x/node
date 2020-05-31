fd859e09e7a3d57e19c561f58cdf1238efc1b20e
cihankarsak <cihankarsak@gmail.com>
2020-05-31 18:19:57 +0300

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send("Welcome to Reset4 home page");
+    res.send("Update");
 })
 
 const port = process.env.PORT || 3000;
