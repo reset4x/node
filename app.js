3c3d199730ff2521a195576af0896aa171d99434
cihankarsak <cihankarsak@gmail.com>
2020-05-31 17:57:11 +0300

    change commit

--- app.js
+++ app.js
@@ -12,14 +12,22 @@ app.listen(port, () =>{
 });
 */
 
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send("Welcome to Reset4 home page");
+})
+
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
     html = fs.readFileSync('index.html');
-
+/*
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
+*/
 
 var server = http.createServer(function (req, res) {
     if (req.method === 'POST') {
