d5d48c9f3dc35b45202ed1625fb84aea7665c54b
cihankarsak <cihankarsak@gmail.com>
2020-05-31 23:50:37 +0300

    update2

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/',(req,res) => {
     res.send("Update");
 })
 
-const port = process.env.PORT || 3000;
-
 app.listen(port, () =>{
-    console.log("Superrrr oldu");
+    console.log("Superrrr oldumuki");
+    console.log("Bence Oldu");
 });
