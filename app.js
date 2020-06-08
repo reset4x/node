d16d1c8e6a7c8128f7b196c638e27725298d0389
cihankarsak <cihankarsak@gmail.com>
2020-06-08 15:28:04 +0300

    update26

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 var http = require('http');
+//Mongoose 'ı ekledik
+var mongoose = require('mongoose');
+var User = require('./user');
+
+
+const uri = 'mongodb+srv://testUser:Test1234@reset4cluster-exgxd.mongodb.net/testDB';
+
+mongoose.connect(uri, {useNewUrlParser: true }).then( () => {
+  console.log('MongoDB ye bağlanti başarili')
+})
+.catch( (err) => console.error(err));
+
+
+
+
 
 /**
  * Dinleyeceğimiz port numarasını aws içindeki environment larda PORT değişkeninden alalım eğer Port değişkeni yok ise default olarak 8081 portunu i dinleyelim.
@@ -7,12 +22,45 @@ var port = normalizePort(process.env.PORT || '8081');
 
 const express = require('express');
 const app = express();
+var bodyparser = require('body-parser');
+
+var urlencoded = bodyparser.urlencoded({extended: false});
 
 /**
  * bir get apisi ile ekrana mesajımızı yazdıralım
  */
 app.get('/',(req,res) => {
-    res.send("Uygulama başarılı ayağa kalktı");
+    res.send("Servis ayağa kalktı");
+})
+
+
+/**
+ * bir test get apisi ile ekrana mesajımızı yazdıralım
+ */
+app.get('/test',(req,res) => {
+  res.send("Test başarılı");
+})
+
+
+/**
+ * bir post apisi ile ekrana mesajımızı yazdıralım
+ */
+app.post('/user',urlencoded,(req,res) => {
+  res.send("User Post isteği alındı");
+})
+
+/**
+ * bir post apisi ile ekrana mesajımızı yazdıralım
+ */
+app.post('/create_user',urlencoded,(req,res) => {
+  var user = new User({
+    name: req.body.name,
+    email:req.body.email
+  });
+  
+  user.save();
+
+  res.send(req.body.name + " li User kayit edildi");
 })
 
 /**
